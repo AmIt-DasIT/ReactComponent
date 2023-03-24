a19a4b1602c85bcdc4534eca77bd2aa2aac06acd
@iamamitdas <amit@distronix.in>
2023-03-24 11:51:14 +0530

    All components add

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react';
+import { Reorder, motion, progress } from "framer-motion";
+import './Scss/App.scss';
+import AudioPlayer from './components/Audio/AudioPlayer';
+import PageFolding from './components/PageFolding/PageFolding';
+import Menu from './components/Menu/Menu';
+import CardFlip from './components/CardAnimation/CardFlip/CardFlip';
+import ProfileCard from './components/CardAnimation/Card/ProfileCard';
+import Waterdrop from './components/Waterdrop/Waterdrop';
+import DragDrop from './components/DragDrop/DragDrop';
+import Polygon from './components/Polygon/Polygon';
+import Clock from './components/Clock/Clock';
+import CodeWeb from './components/CodeinWeb/CodeWeb';
+import Cube from './components/Cube/Cube';
+import ImageSplit from './components/ImageSplit/ImageSplit';
+import Ribbon from './components/Ribbon/Ribbon';
+import AnimatedWeb from './components/AnimatedWeb/AnimatedWeb';
+import Loader from './components/Loader/Loader';
+import Sun from './components/Sun/Sun';
+import Pendulam from './components/Pendulam/Pendulam';
+import Signup from './components/Signup/Signup';
+import CardTilt from './components/CardAnimation/CardTilt/CardTilt';
+import Online from './components/Online-offline/Online';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      {/* <AudioPlayer /> */}
+      {/* <PageFolding /> */}
+      {/* <Menu /> */}
+      {/* <CardFlip /> */}
+      {/* <ProfileCard /> */}
+      {/* <Waterdrop /> */}
+      {/* <DragDrop /> */}
+      {/* <Polygon /> */}
+      {/* <Clock /> */}
+      {/* <CodeWeb /> */}
+      {/* <Cube /> */}
+      {/* <ImageSplit /> */}
+      {/* <Ribbon /> */}
+      {/* <AnimatedWeb /> */}
+      {/* <Loader /> */}
+      {/* <Sun /> */}
+      {/* <Pendulam /> */}
+      {/* <Signup /> */}
+      {/* <CardTilt /> */}
+      {/* <Online />  */}
     </div>
-  );
+
+  )
 }
 
 export default App;
